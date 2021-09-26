46abef7d5d1a156f11be39346705643134bd606e
Sarang0608 <sarang0608.desai@gmail.com>
2021-09-26 18:56:48 +0530

    Added the Passport JWT Authentication stratergy for Users

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport")
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -26,10 +27,11 @@ mongoose
     console.log(err);
   });
 
-//The main page
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// Passport Middleware
+app.use(passport.initialize())
+
+// Passport Config
+require('./config/passport.js')(passport)
 
 //Use Routes
 app.use("/api/users", users);
