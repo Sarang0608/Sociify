1e25b37d95cf89373eb7903e46531d732dcb3c0f
Sarang837 <f20190594@goa.bits-pilani.ac.in>
2021-09-26 16:42:28 +0530

    Initial Express Server with Route Files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Sarang:Sarang@cluster0.2a3gr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+//Initializing the app
+const app = express();
+
+//Database Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB through Mongoose
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//The main page
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+//Start the server on PORT
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
